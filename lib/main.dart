b82e19750f0f794dfde9f72cfdfd4e4beeae06a8
wemlion <angusfu1126@qq.com>
2018-03-16 00:00:23 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import './ShoppingListItem.dart';
 import './ShoppingListPage.dart';
 import './CounterPage.dart';
 import './ListPage.dart';
@@ -9,7 +8,7 @@ import './AppState.dart';
 import 'package:redux/redux.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
-AppState counterReducer(AppState state, action) {
+AppState appReducer(AppState state, action) {
   switch (action['type']) {
     case 'Actions.Increment':
       state.count += 1;
@@ -28,28 +27,9 @@ void main() {
   runApp(new WemlionDemoApp());
 }
 
-final List<Map<String, String>> routes = [
-  {
-    "label": "List Demo",
-    "route": "/list",
-  },
-  {
-    "label": "Counter Demo",
-    "route": "/counter",
-  },
-  {
-    "label": "Gesture Demo",
-    "route": "/gesture",
-  },
-  {
-    "label": "Shopping List Demo",
-    "route": "/shopping-list",
-  },
-];
-
 class WemlionDemoApp extends StatelessWidget {
   final store = new Store<AppState>(
-    counterReducer,
+    appReducer,
     initialState: new AppState()
   );
 
@@ -62,7 +42,7 @@ class WemlionDemoApp extends StatelessWidget {
         theme: new ThemeData(
           primarySwatch: Colors.blue
         ),
-        home: new IndexPage(routes: routes),
+        home: new IndexPage(),
         routes: <String, WidgetBuilder> {
           '/list': (BuildContext context) => new ListPage(),
           '/counter': (BuildContext context) => new CounterPage(),
@@ -76,11 +56,8 @@ class WemlionDemoApp extends StatelessWidget {
 
 class IndexPage extends StatefulWidget {
   IndexPage({
-    Key key,
-    this.routes
+    Key key
   }) : super(key: key);
-
-  final List<Map<String, String>> routes;
 
   @override
   IndexPageState createState() {
@@ -91,42 +68,49 @@ class IndexPage extends StatefulWidget {
 class IndexPageState extends State<IndexPage> {
   @override
   Widget build(BuildContext build) {
+    StoreConnector body = new StoreConnector<AppState, List<Map<String, String>>>(
+      converter: (store) => store.state.routes,
+      builder: (context, routes) {
+        return new CustomScrollView(
+          shrinkWrap: true,
+          slivers: <Widget>[
+            new SliverPadding(
+              padding: const EdgeInsets.all(20.0),
+              sliver: new SliverList(
+                delegate: new SliverChildListDelegate(
+                  routes.map((route) {
+                    return new ListTile(
+                      onTap: () {
+                        Navigator.of(context).pushNamed(route["route"]);
+                      },
+                      leading: new CircleAvatar(
+                        backgroundColor: Theme.of(context).primaryColor,
+                        child: new Text(route["label"][0])
+                      ),
+                      title: new Text(
+                        route["label"],
+                        style: new TextStyle(
+                          color: Colors.black54
+                        ),
+                        maxLines: 2,
+                        textAlign: TextAlign.left,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    );
+                  }).toList()
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Home'),
       ),
-      body: new CustomScrollView(
-        shrinkWrap: true,
-        slivers: <Widget>[
-          new SliverPadding(
-            padding: const EdgeInsets.all(20.0),
-            sliver: new SliverList(
-              delegate: new SliverChildListDelegate(
-                widget.routes.map((route) {
-                  return new ListTile(
-                    onTap: () {
-                      Navigator.of(context).pushNamed(route["route"]);
-                    },
-                    leading: new CircleAvatar(
-                      backgroundColor: Theme.of(context).primaryColor,
-                      child: new Text(route["label"][0])
-                    ),
-                    title: new Text(
-                      route["label"],
-                      style: new TextStyle(
-                        color: Colors.black54
-                      ),
-                      maxLines: 2,
-                      textAlign: TextAlign.left,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  );
-                }).toList()
-              ),
-            ),
-          ),
-        ],
-      )
+      body: body
     );
   }
 }
